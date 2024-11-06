8788d3125820ddbf58e9727fccd3852217fcd502
marijn <marijn@serraict.com>
2024-11-06 09:23:13 +0100

    refactor: Move web test configuration to its own directory

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,16 +1,1 @@
-"""Test configuration and fixtures."""
-
-from typing import Generator
-import pytest
-from nicegui.testing import User
-
 pytest_plugins = ["nicegui.testing.user_plugin"]
-
-
-@pytest.fixture
-def user(user: User) -> Generator[User, None, None]:
-    """Initialize NiceGUI for testing."""
-    from vineapp.web.startup import startup
-
-    startup()
-    yield user
--- tests/web/conftest.py
+++ tests/web/conftest.py
@@ -1,1 +1,14 @@
-"""Test configuration and fixtures for web tests."""
+"""Test configuration and fixtures."""
+
+from typing import Generator
+import pytest
+from nicegui.testing import User
+
+
+@pytest.fixture
+def user(user: User) -> Generator[User, None, None]:
+    """Initialize NiceGUI for testing."""
+    from vineapp.web.startup import startup
+
+    startup()
+    yield user
