5ab43c8520a04f91e3104e027a4056e72502b9f7
marijn <marijn@serraict.com>
2024-11-15 10:10:41 +0100

    fix(template): add debug output to entrypoint script
    Add set -x to see exactly what commands are being executed.

--- template/{{cookiecutter.project_slug}}/docker-entrypoint.sh
+++ template/{{cookiecutter.project_slug}}/docker-entrypoint.sh
@@ -1,27 +1,12 @@
 #!/bin/bash
+set -x  # Print commands as they are executed
 
 # Print version info
 cliapp version
 echo "Started {{cookiecutter.project_name}} container."
 
 echo "Starting web server..."
-# First check if we can import the module
-if ! python -c "import {{cookiecutter.project_slug}}.__web__"; then
-    echo "Failed to import web module"
-    exit 1
-fi
-
-# Start the web server
 python -m {{cookiecutter.project_slug}}.__web__ 2>&1 | tee -a /var/log/webapp.log &
-WEB_PID=$!
-
-# Check if process is running
-if ps -p $WEB_PID > /dev/null; then
-    echo "Web server process started with PID $WEB_PID"
-else
-    echo "Failed to start web server process"
-    exit 1
-fi
 
 # Keep container running and show logs
 tail -f /var/log/webapp.log
